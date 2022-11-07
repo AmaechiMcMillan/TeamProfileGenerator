536c6b7f9bcdc4fc01f3c65427d8f205c45e658c
Amaechi McMillan <amaechimcmillan@gmail.com>
2022-11-07 10:42:57 -0600

    template working properly

--- src/template.js
+++ src/template.js
@@ -4,62 +4,59 @@ const Intern = require("../lib/Intern");
 const rosterPage = require("../lib/TeamProfile");
 
 const teamPage = (rosterData) => {
-  // const { managerName, managerId, managerEmail, managerOfficeNumber } =
-  //   rosterData;
-  // const newManager = new Manager(
-  //   managerName,
-  //   managerId,
-  //   managerEmail,
-  //   managerOfficeNumber
-  // );
-  // for (let index = 0; index < array.length; index++) {
-
-  // }
-  return `
-    <!DOCTYPE html>
-  <html lang=en>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Team Roster</title>
-    <script src="https://kit.fontawesome.com/5bae9e23d6.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./style.css" />
-  </head>
-  <body>
-    <h1>My Team</h1>
-    <div class="card-container">
-      <div class="card">
-        <div class="card-title">
-          <h3>${rosterData[0].getName()}</h3>
-          <h3><i class="fa-solid fa-person"></i>${rosterData[0].getRole()}</h3>
-        </div>
-        <div class="attribute">Office number: <a href="tel:${rosterData[0].getOfficeNumber()}">${rosterData[0].getOfficeNumber()}</a></div>
-      </div>
-      
-      
-      ${rosterData.forEach(function (classes) {
-        if (classes.getRole() === "Engineer") {
-          return `
+  let rosterCards = "";
+  rosterData.forEach(function (classes) {
+    if (classes.getRole() === "Manager") {
+      rosterCards += `
             <div class="card-container">
       <div class="card">
         <div class="card-title">
-          <h3>${rosterData[0].getName()}</h3>
+          <h3>${classes.getName()}</h3>
           <h3><i class="fa-solid fa-person"></i>${classes.getRole()}</h3>
         </div>
-        <div class="attribute">Office number: <a href="tel:${classes.getGithub()}">${classes.getGithub()}</a></div>
+        <div class="attribute">Office number: <a href="tel:${classes.getOfficeNumber()}">${classes.getOfficeNumber()}</a></div>
       </div>`;
-        } else if (classes.getRole() === "Intern") {
-          return `<div class="card">
+    } else if (classes.getRole() === "Engineer") {
+      rosterCards += `
+            <div class="card-container">
+      <div class="card">
+        <div class="card-title">
+          <h3>${classes.getName()}</h3>
+          <h3><i class="fa-solid fa-person"></i>${classes.getRole()}</h3>
+        </div>
+        <div class="attribute">Email: <a href="mailTo:${classes.getEmail()}">${classes.getEmail()}</a></div>
+        <div class="attribute">Github: <a href="tel:${classes.getGithub()}">${classes.getGithub()}</a></div>
+      </div>`;
+    } else if (classes.getRole() === "Intern") {
+      rosterCards += `<div class="card">
           <div class="card-title">
             <h3>${classes.getName()}</h3>
             <h3><i class="fa-brands fa-dev"></i>${classes.getRole()}</h3>
           </div>
           <div class="attribute">ID: ${classes.getId()}</div>
           <div class="attribute">Email: <a href="mailTo:${classes.getEmail()}">${classes.getEmail()}</a></div>
-          <div class="attribute">GitHub account: <a href="https://github.com/${classes.getSchool()}" target="_blank">${classes.getSchool()}</a></div>
+          <div class="attribute">School: <a href="https://github.com/${classes.getSchool()}" target="_blank">${classes.getSchool()}</a></div>
           </div>`;
-        }
-      })}
+    }
+  });
+
+  return `
+        <!DOCTYPE html>
+      <html lang=en>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Team Roster</title>
+        <script src="https://kit.fontawesome.com/5bae9e23d6.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" />
+        
+      </head>
+      <body>
+        <h1>My Team</h1>
+        <div class="card-container">
+          <div class="card">
+            ${rosterCards}
+          </div>
     </div>
     </body>
     </html>`;
